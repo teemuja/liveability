46127762536cdf3e0495b7ffda083395461a65a2
Teemu Jama <43031759+teemuja@users.noreply.github.com>
2023-11-14 13:31:20 +0000

    added GWR data

--- app/app.py
+++ app/app.py
@@ -12,19 +12,34 @@ st.set_page_config(page_title="LIV", layout="wide")
 st.header("Liveability decomposed")
 st.markdown("Authors: Tianqi Wang, Teemu Jama, Henrikki Tenkanen @[Aalto GIST Lab](https://gistlab.science)")
 
-df = utils.bucket_handler(operation="download",folder_name='ndp/liv', file_name='liveability_4326.csv')
-df['geometry'] = df['WKT'].apply(loads)
-gdf = gpd.GeoDataFrame(df,geometry="geometry")
-#rename
-json_path = Path(__file__).parent / 'cols_dict.json'
-with open(json_path) as json_file:
-    cols_dict = json.load(json_file)
-gdf.rename(columns=cols_dict,inplace=True)
+@st.cache_data()
+def get_gdf(folder_name='ndp/liv', file_name='liveability_4326.csv'):
+    df = utils.bucket_handler(operation="download",folder_name=folder_name,file_name=file_name)
+    df['geometry'] = df['WKT'].apply(loads)
+    gdf = gpd.GeoDataFrame(df,geometry="geometry")
+    return gdf
+
+gdf_indexes = get_gdf(folder_name='ndp/liv', file_name='liveability_4326.csv')
+gdf_GWR = get_gdf(folder_name='ndp/liv', file_name='GWR.csv')
 
 #selectors
-cols = gdf.drop(columns=["YKR_ID","WKT","geometry"]).columns.tolist()
-defaults = ["Livability Index","Walkabilit"]
-mycols = st.multiselect("Select values to study",options=cols,default=defaults,max_selections=4)
+df_to_study = st.radio('Select data to study',options=['Indexes','GWR'])
+
+if df_to_study == "Indexes":
+    cols = gdf_indexes.drop(columns=["YKR_ID","WKT","geometry"]).columns.tolist()
+    defaults = [cols[0],cols[1]]
+    mycols = st.multiselect("Select values to study",options=cols,default=defaults,max_selections=4)
+    gdf = gdf_indexes.copy()
+    #rename cols
+    json_path = Path(__file__).parent / 'cols_dict.json'
+    with open(json_path) as json_file:
+        cols_dict = json.load(json_file)
+    gdf.rename(columns=cols_dict,inplace=True)
+else:
+    cols = gdf_GWR.drop(columns=["YKR_ID","WKT","geometry"]).columns.tolist()
+    defaults = [cols[0],cols[1]]
+    mycols = st.multiselect("Select values to study",options=cols,default=defaults,max_selections=4)
+    gdf = gdf_GWR.copy()
 
 #tabs
 tab1,tab2 = st.tabs(['Maps','Distribution'])
